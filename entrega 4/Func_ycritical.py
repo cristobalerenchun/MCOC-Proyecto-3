d92739fa6e5cdf5b9bd91818dbd596e1c3d34637
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-05 13:46:13 -0300

    Update Func_ycritical.py

--- entrega 4/Func_ycritical.py
+++ entrega 4/Func_ycritical.py
@@ -3,20 +3,25 @@
 #FUNCIÓN PARA HALLAR LA ALTURA CRITICA DE UN CANAL TRAPEZOIDAL
 #USANDO NEWTON-RAPHSON (NR)
 
-# Q   >>>  [m^3*s^-1] Caudal que debe transportar la sección
-# C_0 >>>  [SI=>1 - SB=>1.49] Factor de conversión unidades
+# Q   >>>  [m^3*s^-1 o pie^3*s^-1] Caudal que debe transportar la sección
+# g   >>>  Gravedad nominal terrestre [SI=> 9.81 m*s^-2 - SB=> 32.2 pie*s^-2]
 # z   >>>  Relación Horizontal/Vertical del talud
-# b   >>>  [m] Ancho del fondo del canal
+# b   >>>  [m o pie] Ancho del fondo del canal
 
 def ycritical(C_0, z, b, Q):       # Definición de función ycritical
     yc = 1.0                       # Altura inicial para proceso iterativo
+     #Condición del sistema de unidades (SI o SB)
+    if Units == "SI":
+        g  = 9.81
+    else:
+        g  = 32.2
     #Función de solución
     def D(y):                      # Definición de función argumento NR
         global A, P
         #Variables de calculo
-        k   = Q**2/9.81            # [m^2*s] Constante del modelo 
-        A   = y*(b + z*y)          # [m^2] Área mojada
-        T   = b + 2*z*y            # [m] Derivada total de A respecto a y
+        k   = Q**2/9.81            # [m^5 o pie^5] Constante del modelo 
+        A   = y*(b + z*y)          # [m^2 o pie^2] Área mojada
+        T   = b + 2*z*y            # [m o pie] Derivada total de A respecto a y
         d_T = 2*z                  # Derivada total de T respecto a y   
         #Función solución y su derivada
         f   = A**3 - k*T           # Función objetivo f(y) = 0
@@ -28,10 +33,10 @@ def ycritical(C_0, z, b, Q):       # Definición de función ycritical
         yc1 = yc - D(yc)           # Ecuación de NR
         tol = abs(yc1-yc)          # Tolerancia del paso
         yc  = yc1                  # Mutación de yn
-    return yc
+    return yc                      # [m o pie] altura critica
 
 #EJEMPLOS DE APLICACIÓN:
 #Solución
-print "Canal 1, yc =",ycritical(1.0, 1.25, 9.10, 38.53),"m"
-print "Canal 2, yc =",ycritical(1.0, 8.0, 0.0, 7.2),"m"
-print "Canal 3, yc =",ycritical(1.0, 0.0, 0.50, 0.15),"m"
+print "Canal 1, yc =",ycritical("SI", 1.25, 9.10, 38.53)
+print "Canal 2, yc =",ycritical("SI", 8.0, 0.0, 7.2)
+print "Canal 3, yc =",ycritical("SI", 0.0, 0.50, 0.15)
