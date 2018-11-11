84f3a6af15e276c223fbad19a667ed942881a48f
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-11 16:40:26 -0300

    Update Func_geom.py

--- entrega 5/Func_geom.py
+++ entrega 5/Func_geom.py
@@ -1,11 +1,20 @@
-import scipy as sp
-def geom(y,B,ss):
-	A = B*y + ss*(y**2)
-	dA = B + 2*y*ss
-	P = B + 2*y*sp.sqrt((ss**2)+1)
-	dP = 2*sp.sqrt((ss**2)+1)
-	R = A/P
-	dR = (2*(y**2)*ss*sp.sqrt((ss)**2+1)+2*B*y*ss+(B**2))/((2*y*sp.sqrt(ss*2+1)+B)**2)
-#	dF = (5/3)*(A**(4/3))*(P**(-2/3))*dA-(2/3)*(A**(5/3))*(P**(-5/3))*dP
-	d = { 'A' : A , 'dA' : dA , 'P':P, 'dP':dp,'R':R,'dR':dR}
-	return d
+# -*- coding: utf-8 -*-
+
+# FUNCIÓN PARA CALCULAR LOS PARÁMETROS GEOMÉTRICOS
+# DE UNA SECCIÓN TRAPEZOIDAL SIMÉTRICA GENERAL
+
+# Argumentos de la función:
+# b >>> [m o pie] Ancho del fondo del canal
+# z >>> Relación Horizontal/Vertical del talud lateral
+# y >>> [m o pie] Profundidad del agua
+
+def geom(b, z, y):
+	# Parámetros geométricos:
+	A  = y*(b + y*z)				# [m^2 o pie^2] Área mojada
+	T  = b + 2.*y*z					# [m o pie] Ancho superficial
+	P  = b + 2.*y*(z**2.+1.)**0.5			# [m o pie] Perímetro mojado
+	dP = 2.*(z**2.+1)**0.5				# Derivada total de T respecto a y
+	R  = A/P 					# [m o pie] Radio hidráulico
+	D  = A/T 					# [m o pie] Profundidad hidráulica
+	# Guardado parámetros en diccionario:
+	return {'A':A ,'T':T ,'P':P, 'dP/dy':dP,'R':R, 'D':D}
