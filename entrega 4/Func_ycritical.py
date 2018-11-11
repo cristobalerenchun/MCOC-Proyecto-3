32a34e5612f0547e7e651f868ee6cc743c75502d
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-11 16:21:32 -0300

    Update Func_ycritical.py

--- entrega 4/Func_ycritical.py
+++ entrega 4/Func_ycritical.py
@@ -9,22 +9,22 @@
 # b     >>>  [m o pie] Ancho del fondo del canal
 # Q     >>>  [m^3*s^-1 o pie^3*s^-1] Caudal transporta la sección
 
-def ycritical(units, z, b, Q):     # Definición de función ycritical
+def ycritical(units, z, b, Q):
     yc = 1.0                       # Altura inicial para proceso iterativo
-     #Condición del sistema de unidades (SI o SB)
+     # Condición del sistema de unidades (SI o SB)
     if units == "SI":
         g  = 9.81                  # [SI=> 9.81 m*s^-2] Gravedad nominal terrestre
     else:
         g  = 32.2                  # [SB=> 32.2 pie*s^-2] Gravedad nominal terrestre
-    #Función de solución
+    # Función de aproximación:
     def D(y):                      # Definición de función argumento NR
         global A, P
-        #Variables de calculo
+        # Parámetros de calculo:
         k   = Q**2./g              # [m^5 o pie^5] Constante del modelo 
         A   = y*(b + z*y)          # [m^2 o pie^2] Área mojada
         T   = b + 2.*z*y           # [m o pie] Ancho superficial (dA/dy)
         dT  = 2.*z                 # Derivada total de T respecto a y (dT/dy)  
-        #Función objetivo y su primera derivada total
+        # Función objetivo y su primera derivada total
         f   = A**3. - k*T          # Función objetivo f(y) = 0
         df  = 3.*A**2.*T - k*dT    # Derivada total  df(y)/dy
         return f/df                # Segundo termino de formula de NR
@@ -36,8 +36,8 @@ def ycritical(units, z, b, Q):     # Definición de función ycritical
         yc  = yc1                  # Mutación de y
     return yc                      # [m o pie] altura critica
 
-#EJEMPLOS DE APLICACIÓN:
-#Solución
+# EJEMPLOS DE APLICACIÓN
+# Solución:
 print "Canal 1, yc =",ycritical("SI", 1.25, 9.10, 38.53)
 print "Canal 2, yc =",ycritical("SI", 8.0, 0.0, 7.2)
 print "Canal 3, yc =",ycritical("SI", 0.0, 0.50, 0.15)
