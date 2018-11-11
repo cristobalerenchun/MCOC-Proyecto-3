727f54cf1b2108f483f7e6b06574c3fb73dafd1e
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-11 16:05:53 -0300

    Update Func_ycritical.py

--- entrega 4/Func_ycritical.py
+++ entrega 4/Func_ycritical.py
@@ -4,10 +4,10 @@
 #USANDO NEWTON-RAPHSON (NR)
 
 # Argumentos de la función:
-# units >  [SI=> g = 9.91m*s^-2 - SB=> g = 32.2 pie*s^-2]
-# z   >>>  Relación Horizontal/Vertical del talud lateral
-# b   >>>  [m o pie] Ancho del fondo del canal
-# Q   >>>  [m^3*s^-1 o pie^3*s^-1] Caudal transporta la sección
+# units >>>  [SI=> g = 9.91 m*s^-2 - SB=> g = 32.2 pie*s^-2]
+# z     >>>  Relación Horizontal/Vertical del talud lateral
+# b     >>>  [m o pie] Ancho del fondo del canal
+# Q     >>>  [m^3*s^-1 o pie^3*s^-1] Caudal transporta la sección
 
 def ycritical(units, z, b, Q):     # Definición de función ycritical
     yc = 1.0                       # Altura inicial para proceso iterativo
@@ -23,11 +23,11 @@ def ycritical(units, z, b, Q):     # Definición de función ycritical
         k   = Q**2./g              # [m^5 o pie^5] Constante del modelo 
         A   = y*(b + z*y)          # [m^2 o pie^2] Área mojada
         T   = b + 2.*z*y           # [m o pie] Ancho superficial (dA/dy)
-        d_T = 2.*z                 # Derivada total de T respecto a y   
+        dT  = 2.*z                 # Derivada total de T respecto a y (dT/dy)  
         #Función objetivo y su primera derivada total
         f   = A**3. - k*T          # Función objetivo f(y) = 0
-        d_F = 3.*A**2.*T - k*d_T   # Derivada total  f'(y)
-        return f/d_F               # Segundo termino de formula de NR
+        df  = 3.*A**2.*T - k*dT    # Derivada total  df(y)/dy
+        return f/df                # Segundo termino de formula de NR
     # Proceso de aproximaciones sucesivas
     tol = abs(D(yc))               # Tolerancia iteración 1
     while tol > 1e-6:
