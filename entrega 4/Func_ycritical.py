a197f6f97a54a9b72bf653bea7ad946899c4789e
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-11 15:41:48 -0300

    Update Func_ycritical.py

--- entrega 4/Func_ycritical.py
+++ entrega 4/Func_ycritical.py
@@ -3,36 +3,37 @@
 #FUNCIÓN PARA HALLAR LA ALTURA CRITICA DE UN CANAL TRAPEZOIDAL
 #USANDO NEWTON-RAPHSON (NR)
 
-# Q   >>>  [m^3*s^-1 o pie^3*s^-1] Caudal que debe transportar la sección
-# g   >>>  Gravedad nominal terrestre [SI=> 9.81 m*s^-2 - SB=> 32.2 pie*s^-2]
-# z   >>>  Relación Horizontal/Vertical del talud
+# Argumentos de la función:
+# units >  [SI=> g = 9.91m*s^-2 - SB=> g = 32.2 pie*s^-2]
+# z   >>>  Relación Horizontal/Vertical del talud lateral
 # b   >>>  [m o pie] Ancho del fondo del canal
+# Q   >>>  [m^3*s^-1 o pie^3*s^-1] Caudal transporta la sección
 
-def ycritical(Units, z, b, Q):       # Definición de función ycritical
+def ycritical(units, z, b, Q):     # Definición de función ycritical
     yc = 1.0                       # Altura inicial para proceso iterativo
      #Condición del sistema de unidades (SI o SB)
-    if Units == "SI":
-        g  = 9.81
+    if units == "SI":
+        g  = 9.81                  # [SI=> 9.81 m*s^-2] Gravedad nominal terrestre
     else:
-        g  = 32.2
+        g  = 32.2                  # [SB=> 32.2 pie*s^-2] Gravedad nominal terrestre
     #Función de solución
     def D(y):                      # Definición de función argumento NR
         global A, P
         #Variables de calculo
-        k   = Q**2/9.81            # [m^5 o pie^5] Constante del modelo 
+        k   = Q**2./g              # [m^5 o pie^5] Constante del modelo 
         A   = y*(b + z*y)          # [m^2 o pie^2] Área mojada
-        T   = b + 2*z*y            # [m o pie] Derivada total de A respecto a y
-        d_T = 2*z                  # Derivada total de T respecto a y   
-        #Función solución y su derivada
-        f   = A**3 - k*T           # Función objetivo f(y) = 0
-        d_F = 3*A**2*T - k*d_T     # Derivada total  f'(y)
-        return f/d_F               # Segundo termino de ecuación de NR
+        T   = b + 2.*z*y           # [m o pie] Ancho superficial (dA/dy)
+        d_T = 2.*z                 # Derivada total de T respecto a y   
+        #Función objetivo y su primera derivada total
+        f   = A**3. - k*T          # Función objetivo f(y) = 0
+        d_F = 3.*A**2.*T - k*d_T   # Derivada total  f'(y)
+        return f/d_F               # Segundo termino de formula de NR
     # Proceso de aproximaciones sucesivas
     tol = abs(D(yc))               # Tolerancia iteración 1
     while tol > 1e-6:
-        yc1 = yc - D(yc)           # Ecuación de NR
+        yc1 = yc - D(yc)           # Formula de NR
         tol = abs(yc1-yc)          # Tolerancia del paso
-        yc  = yc1                  # Mutación de yn
+        yc  = yc1                  # Mutación de y
     return yc                      # [m o pie] altura critica
 
 #EJEMPLOS DE APLICACIÓN:
