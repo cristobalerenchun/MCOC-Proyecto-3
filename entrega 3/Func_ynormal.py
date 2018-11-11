287f36082485ba751e6a9ab4f3a70dc4b19d9e37
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-11 15:54:36 -0300

    Update Func_ynormal.py

--- entrega 3/Func_ynormal.py
+++ entrega 3/Func_ynormal.py
@@ -4,20 +4,20 @@
 #USANDO NEWTON-RAPHSON (NR)
 
 # Argumentos de la función:
-# units >>>[SI=> C_0 = 1.00 - SB=>C_0 = 1.49]
+# units >>>  [SI=> C_0 = 1.00 - SB=>C_0 = 1.49]
 # n     >>>  [s*m^(-1/3) o pie^3*s^-1] Coeficiente rugosidad de Manning
 # S     >>>  [%] Pendiente longitudinal
 # z     >>>  Relación Horizontal/Vertical del talud
 # b     >>>  [m o pie] Ancho del fondo del canal
 # Q     >>>  [m^3*s^-1 o pie^3*s^-1] Caudal que debe transportar la sección
 
-def ynormal(Units, n, S, z, b, Q):         # Definición de función ynormal
+def ynormal(units, n, S, z, b, Q):         # Definición de función ynormal
     yn = 1.0                               # Altura inicial para proceso iterativo
     # Condición del sistema de unidades (SI o SB)
-    if Units == "SI":
-        C_0  = 1.00                  
+    if units == "SI":
+        C_0  = 1.00                        # Factor de conversión unidades (SI)
     else:
-        C_0  = 1.49 
+        C_0  = 1.49                        # Factor de conversión unidades (SB)
     # Función objetivo
     def D(y):                              # Definición de función argumento NR
         global A, P
@@ -25,8 +25,8 @@ def ynormal(Units, n, S, z, b, Q):         # Definición de función ynormal
         k   = (10.*Q*n/(C_0*S**0.5))**3.   # [m^8 o pie^8] Constante del modelo hidráulico 
         A   = y*(b + z*y)                  # [m^2 o pie^2] Área mojada
         P   = b + 2.*y*(z**2.+1.)**0.5     # [m o pie] Perímetro mojado
-        T   = b + 2.*z*y                   # [m o pie] Derivada total de A respecto a y
-        d_P = 2*(z**2+1)**0.5              # Derivada total de P respecto a y   
+        T   = b + 2.*z*y                   # [m o pie] Ancho superficial (dA/dy)
+        d_P = 2.*(z**2.+1)**0.5            # Derivada total de P respecto a y   
         # Función objetivo y su derivada total
         f   = A**5. - k*P**2.              # Función objetivo f(y)=0
         d_F = 5.*A**4.*T - 2.*k*P*d_P      # Derivada total  f'(y)
