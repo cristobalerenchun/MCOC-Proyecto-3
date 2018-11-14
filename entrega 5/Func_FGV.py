3f0874fedb8013396562537273ae8a16c47ad678
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-14 02:40:43 -0300

    Update Func_FGV.py

--- entrega 5/Func_FGV.py
+++ entrega 5/Func_FGV.py
@@ -27,27 +27,27 @@ def FGV(units, b, z, n, Q, S, dx, control, y_control, elev_control):
 	global X, Z
 	yn = ynormal(units, n, S, z, b, Q)			# Altura normal yn
 	# Valores iniciales de los vectores:
-	x0 = 0.										# Distancia inicial
-	y0 = y_control								# Altura de agua conocida e incial
-	z0 = elev_control							# Elevación inicial en sección de control
+	x0 = 0.							# Distancia inicial
+	y0 = y_control						# Altura de agua conocida e incial
+	z0 = elev_control					# Elevación inicial en sección de control
 	# Listas para guardado de datos:
-	X  = [x0]									# Inicializando lista de distancia horizontal
-	Y  = [y0]									# Inicializando lista de alturas del agua
-	Z  = [z0]									# Inicializando lista elevación fondo del canal
+	X  = [x0]						# Inicializando lista de distancia horizontal
+	Y  = [y0]						# Inicializando lista de alturas del agua
+	Z  = [z0]						# Inicializando lista elevación fondo del canal
 	# Generando listas de datos:
-	i  = 0										# Indice de la lista
-	dy = 1e-6									# Criterio de finalización del proceso
+	i  = 0							# Indice de la lista
+	dy = 1e-6						# Criterio de finalización del proceso
 	while Y[i] < yn - dy or Y[i] > yn + dy:
 		# Ubicación del control de flujo:
-		if control == 'DS':						# Control aguas abajo [Downstream]
+		if control == 'DS':				# Control aguas abajo [Downstream]
 			a = -1.
-		else:									# Control aguas arriba [Upstream]
+		else:						# Control aguas arriba [Upstream]
 			a = +1.
 		# Calculando datos para cada estación i+1:
-		X.append(X[i] + dx)											# Distancia horizontal
+		X.append(X[i] + dx)						# Distancia horizontal
 		Y.append(NR_FGV(units, b, z, Y[i], n, Q, S, dx, control))	# Alturas del agua
-		Z.append(Z[i] - a*S*dx)										# Elevacion fondo del canal
-		i  = i + 1													# Mutación del índice
+		Z.append(Z[i] - a*S*dx)						# Elevacion fondo del canal
+		i  = i + 1							# Mutación del índice
 	# Calculo de la elevación superficie del agua:
 	elsa = array(Y) + array(Z)					
-	return elsa									# Vector de elevación de la superficie del agua
+	return elsa								# Vector de elevación de la superficie del agua
