b40ccc7595efccdebd19fac0b54c192f9ef41e94
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-16 12:10:20 -0300

    Update Func_gfv.py

--- entrega 6/Func_gfv.py
+++ entrega 6/Func_gfv.py
@@ -16,37 +16,40 @@
 # elev_control > [m o pie] Elevación en la sección de control
 
 # Funciones auxiliares:
-from numpy        import array
-from matplotlib import pyplot
+from numpy       import array
 from Func_NR_FGV import NR_FGV
-# Argumentos = NR_FGV(units, b, z, y1, n, Q, S, dx, control)
-from Func_ynormal import ynormal
-# Argumentos = ynormal(units, n, S, z, b, Q)
+	# Argumentos = (units, b, z, y1, n, Q, S, dx, control)
 
-def gfv(units, b, z, n, Q, S, dx, control, y_control, elev_control):
-	yn = ynormal(units, n, S, z, b, Q)			# Altura normal yn
+def gfv(units, b, z, n, Q, S, dx, control, y_control, elev_control, y_final):
 	# Valores iniciales de los vectores:
-	x0 = 0.										# Distancia inicial
-	y0 = y_control								# Altura de agua conocida e incial
-	z0 = elev_control							# Elevación inicial en sección de control
+	x0 = 0.					# Distancia inicial
+	y0 = y_control				# Altura de agua conocida e incial
+	z0 = elev_control			# Elevación inicial en sección de control
 	# Listas para guardado de datos:
-	X  = [x0]									# Inicializando lista de distancia horizontal
-	Y  = [y0]									# Inicializando lista de alturas del agua
-	Z  = [z0]									# Inicializando lista elevación fondo del canal
+	X  = [x0]				# Inicializando lista de distancia horizontal
+	Y  = [y0]				# Inicializando lista de alturas del agua
+	Z  = [z0]				# Inicializando lista elevación fondo del canal
+	# Ubicación del control de flujo:
+	if control == 'DS':			# Control aguas abajo [Downstream: 'DS']
+		a = -1.
+	else:					# Control aguas arriba [Upstream: 'US']
+		a = +1.
 	# Generando listas de datos:
-	i  = 0										# Indice de la lista
-	dy = 1e-6									# Criterio de finalización del proceso
-	while Y[i] < yn - dy or Y[i] > yn + dy:
-		# Ubicación del control de flujo:
-		if control == 'DS':						# Control aguas abajo [Downstream]
-			a = -1.
-		else:									# Control aguas arriba [Upstream]
-			a = +1.
+	i  = 0					# Indice de la lista
+	dy = 1e-6				# Criterio de finalización del proceso
+	criterio = True				# Criterio de inicio del proceso
+
+	while criterio and i < 1e6:
 		# Calculando datos para cada estación i+1:
-		X.append(X[i] + dx)											# Distancia horizontal
+		X.append(X[i] + dx)						# Distancia horizontal
 		Y.append(NR_FGV(units, b, z, Y[i], n, Q, S, dx, control))	# Alturas del agua
-		Z.append(Z[i] - a*S*dx)										# Elevacion fondo del canal
-		i  = i + 1													# Mutación del índice
-	# Calculo de la elevación superficie del agua:
+		Z.append(Z[i] - a*S*dx)						# Elevacion fondo del canal
+		i  = i + 1							# Mutación del índice
+		# Evaluando el criterio del proceso:
+		if y_control < y_final:
+			criterio = Y[i] < y_final
+		else:
+			criterio = Y[i] > y_final + dy
+	# Elevación superficie del agua:
 	elsa = array(Y) + array(Z)					
-	return [X, Y, Z, elsa]							# Lista de resultados
+	return [X, Y, Z, elsa]	# Notese que la función proporciona una lista de tres (3) "columnas"
