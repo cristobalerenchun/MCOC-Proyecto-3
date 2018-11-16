f1605526d59a2ea671a0dccb5e9991b96baef858
Harvey-Hernandez <42277965+Harvey-Hernandez@users.noreply.github.com>
2018-11-16 12:07:49 -0300

    Update Func_geom.py

--- entrega 6/Func_geom.py
+++ entrega 6/Func_geom.py
@@ -10,11 +10,11 @@
 
 def geom(b, z, y):
 	# Parámetros geométricos:
-	A  = y*(b + y*z)				# [m^2 o pie^2] Área mojada
-	T  = b + 2.*y*z					# [m o pie] Ancho superficial
+	A  = y*(b + y*z)		# [m^2 o pie^2] Área mojada
+	T  = b + 2.*y*z			# [m o pie] Ancho superficial
 	P  = b + 2.*y*(z**2.+1.)**0.5	# [m o pie] Perímetro mojado
-	dP = 2.*(z**2.+1)**0.5			# Derivada total de T respecto a y
-	R  = A/P 					    # [m o pie] Radio hidráulico
-	D  = A/T 					    # [m o pie] Profundidad hidráulica
+	dP = 2.*(z**2.+1)**0.5		# Derivada total de T respecto a y
+	R  = A/P 			# [m o pie] Radio hidráulico
+	D  = A/T 			# [m o pie] Profundidad hidráulica
 	# Guardado parámetros en diccionario:
 	return {'A':A ,'T':T ,'P':P, 'dP/dy':dP,'R':R, 'D':D}
