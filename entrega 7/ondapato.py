b0f325f8b2177726d94f97ca360af7da957964c2
erenchun <cerenchun@miuandes.cl>
2018-11-19 15:29:53 -0300

    onda

---
+++ entrega 7/ondapato.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 10:56:32 2018
+
+@author: erenchun
+"""
+#seccion1
+N=1000
+L=150000
+B=100
+S=0.001
+n=0.045
+ss=0
+Co=1.485
+NC=1 #puede ser un numero menor a 1 tambien 
+tfin=600
+tfin=tfin*600
+g=32.3
+#seccion2
+dx=L/(N-1)
+
+for i in range(1, N):
+    
+    x(i)=(i-1)*dx
+   
+   
+#seccion2
+   
+
+
+
+    
+    
